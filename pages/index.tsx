0dd2e520caf1dc9311d0624a427fd0106ce8dc69
Harpreet Singh <dhanoaharpreet@gmail.com>
2023-11-23 13:11:24 +1100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      [
+      <h1 className="text-3xl font-bold underline text-blue-800">Hello, Harpreet!</h1>,
+      <div className="mt-10">TypeScripts</div>
+      ]
+          )
+  }
